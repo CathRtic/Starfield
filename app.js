c1a1aa5304e59e5e91813c9be0824f6770204bcc
Nikunj Patel <77161352+CathRtic@users.noreply.github.com>
2022-01-20 10:41:38 -0500

    tidying up code

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ var api_key = "Q6LCLptEevXBK9pvwj1GeEsPbM4mmv6ywTmfw0Nc";
 req.open("GET", url + api_key);
 req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
+req.addEventListener("load", function() {
+  if (req.status == 200 && req.readyState == 4) {
+    var response = JSON.parse(req.responseText);
     document.getElementById("title").textContent = response.title;
     document.getElementById("date").textContent = response.date;
     document.getElementById("pic").src = response.hdurl;
     document.getElementById("explanation").textContent = response.explanation;
   }
 })
+
